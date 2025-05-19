2dd9b05ee44043e7485f663183c3819c189069d5
Yahya Al-hamedi <y.hamedi@jisr.net>
2025-05-19 20:32:56 +0300

    Clean up App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,15 @@ import GameBoard from './components/GameBoard'
 import { useState } from 'react'
 import Log from './components/Log'
 import GameOver from './components/GameOver'
-import { initialGameBoard, WINNING_COMBINATIONS } from './helper'
-
-function deriveActivePlayer(gameTurns) {
-  let currentPlayer = 'X'
-
-  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-    currentPlayer = 'O'
-  }
-
-  return currentPlayer
-}
+import { PLAYERS, deriveActivePlayer, deriveGameBoard, deriveWinner } from './helper'
 
 function App() {
   const [gameTurns, setGameTurns] = useState([])
-  const [players, setPlayers] = useState({
-    X: 'Player 1',
-    O: 'Player 2',
-  })
+  const [players, setPlayers] = useState(PLAYERS)
 
-  const activePlater = deriveActivePlayer(gameTurns)
-
-  let gameBoard = [...initialGameBoard.map((array) => [...array])]
-
-  for (const turn of gameTurns) {
-    const { square, player } = turn
-    const { row, col } = square
-    gameBoard[row][col] = player
-  }
-
-  let winner = null
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
-    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
-    const thridSquareSymbol = gameBoard[combination[2].row][combination[2].column]
-
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thridSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol]
-    }
-  }
+  const activePlayer = deriveActivePlayer(gameTurns)
+  const gameBoard = deriveGameBoard(gameTurns)
+  const winner = deriveWinner(gameBoard, players)
 
   const hasDraw = gameTurns.length === 9 && !winner
 
@@ -94,13 +60,13 @@ function App() {
             <Player
               initialName="Player 1"
               symbol="X"
-              isActive={activePlater === 'X'}
+              isActive={activePlayer === 'X'}
               onNameChange={handlePlayerNameChange}
             />
             <Player
               initialName="Player 2"
               symbol="O"
-              isActive={activePlater === 'O'}
+              isActive={activePlayer === 'O'}
               onNameChange={handlePlayerNameChange}
             />
           </ol>
--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,9 @@
-export const initialGameBoard = [
+export const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+}
+
+export const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -46,3 +51,36 @@ export const WINNING_COMBINATIONS = [
     { row: 2, column: 0 },
   ],
 ]
+
+export function deriveActivePlayer(gameTurns) {
+  // If number of turns is even, it's X's turn, otherwise O's turn
+  return gameTurns.length % 2 === 0 ? 'X' : 'O'
+}
+
+export function deriveGameBoard(gameTurns) {
+  const gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])]
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn
+    const { row, col } = square
+    gameBoard[row][col] = player
+  }
+  return gameBoard
+}
+
+export function deriveWinner(gameBoard, players) {
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      return players[firstSquareSymbol]
+    }
+  }
+  return null
+}
